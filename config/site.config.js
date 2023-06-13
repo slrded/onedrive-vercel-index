f34423c3d93cab2c9820b773872e42fffee618a9
slr <101869987+slrded@users.noreply.github.com>
2023-06-13 20:16:00 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://1drive.slrded.tech" target="_blank" rel="noopener noreferrer">slrded</a>. Made with ❤ .',
+    '<a href="https://1drive.slrded.tech" target="_blank" rel="noopener noreferrer">slrded</a>.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
